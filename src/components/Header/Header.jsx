87326f56092d95d02970b3b50540f73803c710e7
VadimKorobec <_s_e_t__@ukr.net>
2023-12-14 14:14:56 +0200

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,17 +2,20 @@ import styles from "./Header.module.scss";
 import headerSvg from "../../assets/icon/setting.svg";
 import { ListMenu } from "../ListMenu/ListMenu";
 import { User } from "../User/User";
+import { Container } from "../Container/Container";
 
 export const Header = () => {
   return (
-    <div className={styles.header__container}>
-      <div className={styles.title__wrapper}>
-        <img className={styles.title__icon} src={headerSvg} alt="icon" />
-        <h1 className={styles.header__title}>Dashboard</h1>
-        <p className={styles.header__text}>v.01</p>
+    <Container>
+      <div className={styles.header__container}>
+        <div className={styles.title__wrapper}>
+          <img className={styles.title__icon} src={headerSvg} alt="icon" />
+          <h1 className={styles.header__title}>Dashboard</h1>
+          <p className={styles.header__text}>v.01</p>
+        </div>
+        <ListMenu />
+        <User />
       </div>
-      <ListMenu />
-      <User />
-    </div>
+    </Container>
   );
 };
