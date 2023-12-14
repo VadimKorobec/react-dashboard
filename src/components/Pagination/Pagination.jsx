a6c5da54359defe8496fc4c74afb8e2996ccaf43
VadimKorobec <_s_e_t__@ukr.net>
2023-12-14 13:04:00 +0200

    Create Pagination.jsx

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,29 @@
+import styles from "./Pagination.module.scss";
+import { ReactComponent as IconRight } from "../../assets/icon/_.svg";
+import { ReactComponent as IconLeft } from "../../assets/icon/_ (1).svg";
+
+export const Pagination = () => {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.pagination__title}>
+        Showing data 1 to 8 of 256K entries
+      </h2>
+      <div className={styles.pagination__wrapper}>
+        <div className={styles.pagitation__item}>
+          <IconLeft />
+        </div>
+        <ul className={styles.pagination__list}>
+          <li className={styles.pagitation__item}>1</li>
+          <li className={styles.pagitation__item}>2</li>
+          <li className={styles.pagitation__item}>3</li>
+          <li className={styles.pagitation__item}>4</li>
+          <li>...</li>
+          <li className={styles.pagitation__item}>40</li>
+        </ul>
+        <div className={styles.pagitation__item}>
+          <IconRight />
+        </div>
+      </div>
+    </div>
+  );
+};
